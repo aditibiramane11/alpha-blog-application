44711223c9b1dfd5ae9beb6ae07c51ea6b237c73
aditi11 <aditi.biramane@definelabs.com>
2023-09-18 18:47:08 +0530

    install require moudles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+
+
 	def show
-		@article = Article.find(params[:id])
+		#@article = Article.find(params[:id])
 	end
 	def index
 		@articles = Article.all
@@ -11,32 +15,47 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-		@article = Article.find(params[:id])
+		#@article = Article.find(params[:id])
 	end
 
 		
 
 	def create
-		@article = Article.new(params.require(:article).permit(:Title , :Description))
+		@article = Article.new(article_params)
 		if @article.save
 			flash[:notice]="Article was created sucessfully"
 			redirect_to @article
 		else
-			render 'new' 
+			  render "new", status: :unprocessable_entity
 	    end
 	end
 
 	def update
-		@article = Article.find(params[:id])
-		if @article.update(params.require(:article).permit(:Title , :Description))
+		#@article = Article.find(params[:id])
+		if @article.update(article_params)
 			flash[:notice]="Article was edited sucessfully"
 			redirect_to @article
 		else
-			render 'edit'
+			  render "edit", status: :unprocessable_entity
 		end	
+	end
+
+	def destroy
+		#@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
 
 	end
 
+    private
+
+	def set_article
+		@article = Article.find(params[:id])
+	end	
+
+	def article_params
+		params.require(:article).permit(:Title , :Description)
+	end
 end
 
 
