1bc37136da0d9ffaf2b056409bd71304b06dc8b1
aditi11 <aditi.biramane@definelabs.com>
2023-09-21 17:18:38 +0530

    create users table and user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_15_080548) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_21_114024) do
   create_table "articles", force: :cascade do |t|
     t.string "Title"
     t.text "Description"
     t.datetime "created_at"
     t.datetime "updates_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
