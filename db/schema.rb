6195f3cb8c92eb9443800e7f2d97efaa9cda694c
aditi11 <aditi.biramane@definelabs.com>
2023-09-22 17:26:20 +0530

    Created Signup form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_22_055522) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_22_104855) do
   create_table "articles", force: :cascade do |t|
     t.string "Title"
     t.text "Description"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_22_055522) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
