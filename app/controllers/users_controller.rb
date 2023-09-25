799c729d2689971a18150f6b9c063cf1db18496d
aditi11 <aditi.biramane@definelabs.com>
2023-09-25 13:05:19 +0530

    add edit templete and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,26 @@ class UsersController < ApplicationController
     	if  @user.save
     		flash[:notice] = "Welcome to the alpha blog #{@user.username}, you have successfully signed up"
     		redirect_to articles_path
-
         else
-
           render 'new'
         end  	
      end
+
+     def edit
+     	@user = User.find(params[:id])
+
+     end
+
+     def update
+     	@user = User.find(params[:id])
+     	if @user.update(user_params)
+     		flash[:notice] = "Your account information was successfully updated"
+    		redirect_to articles_path
+
+        else
+           render 'edit'
+        end		
+     end 	
 
     private
 
